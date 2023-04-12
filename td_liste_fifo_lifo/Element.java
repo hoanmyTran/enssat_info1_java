8c42c3975a93af2758fff5f5926c3979a366725c
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2023-04-12 14:52:48 +0200

    Update element

--- td_liste_fifo_lifo/Element.java
+++ td_liste_fifo_lifo/Element.java
@@ -1,5 +1,30 @@
 public class Element {
+    private Element suivant;
+    private int value;
+
+    public Element(Element suivant, int value) {
+        this.suivant = suivant;
+        this.value = value;
+    }
+
+    public void setSuivant(Element nouveau) {
+        suivant = nouveau;
+    }
+
+    public Element getSuivant() {
+        return suivant;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return ""+this.getValue();
+    }
+
     public static void main(String[] args) {
-        System.out.println("toto");
+        System.out.println(new Element(null, 5));
     }
 }
