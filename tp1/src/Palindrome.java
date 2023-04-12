5f5e45c31082f67e2c8d5e19f1c6c900bb1f8ad1
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2023-04-12 16:32:44 +0200

    Update palindrome

--- tp1/src/Palindrome.java
+++ tp1/src/Palindrome.java
@@ -1,17 +1,31 @@
+import java.text.Normalizer;
+
 public abstract class Palindrome {
 
-    public static boolean verify(String s1) {
-        String[] strArray = s1.split(" ");
+    public static boolean verify(String s) {
+        String[] strArray = s.split(" ");
         for(int i = 0; i < strArray.length; i++) {
-            StringBuilder sr = new StringBuilder(strArray[i].toLowerCase());
-            if (sr.reverse().toString().equals(strArray[i].toLowerCase())) {
+            StringBuilder str = new StringBuilder(strArray[i].toLowerCase());
+            if (str.toString().equals(str.reverse().toString())) {
                 return true;
             }
         }
         return false;
     }
 
+    public static boolean verifyV2(String s) {
+        StringBuilder str = new StringBuilder(Palindrome.removeAccent(s.toLowerCase().trim().replaceAll("\\s", "")));
+        return str.toString().equals(str.reverse().toString());
+    }
+
+    public static String removeAccent(String s) {
+        return Normalizer.normalize(s, Normalizer.Form.NFD).replaceAll("[\u0300-\u036F]", "");
+    }
+
     public static void main(String[] args) {
         System.out.println(Palindrome.verify("Anna est un palindrome"));
+        System.out.println(Palindrome.verify("anna est un palindrome"));
+        System.out.println(Palindrome.verifyV2("élu par cette crapule"));
+        System.out.println(Palindrome.verifyV2("ésope reste ici et se repose"));
     }
 }
