473372961442210d8c8af3eebe06639a274764a9
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2023-04-13 11:50:51 +0200

    Update toString and afficherListe functions

--- td_liste_fifo_lifo/ListeFIFO.java
+++ td_liste_fifo_lifo/ListeFIFO.java
@@ -8,4 +8,35 @@ public class ListeFIFO {
     public boolean estVide() {
         return debut == null;
     }
+
+    public void empiler(Element nouveau) {
+        if (this.estVide())
+            debut = nouveau;
+        else {
+            Element element = this.debut;
+            while(element.getSuivant() != null) {
+                element = element.getSuivant();
+            }
+            element.setSuivant(nouveau);
+        }
+    }
+
+    public void afficherListe() {
+        System.out.println(this);
+    }
+    @Override
+    public String toString() {
+        if (this.estVide()) {
+            return "Liste vide";
+        } else {
+            Element e = this.debut;
+            StringBuilder s = new StringBuilder(e.toString());
+            while(e.getSuivant() != null) {
+                e = e.getSuivant();
+                s.append(" | ");
+                s.append(e);
+            }
+            return s.toString();
+        }
+    }
 }
