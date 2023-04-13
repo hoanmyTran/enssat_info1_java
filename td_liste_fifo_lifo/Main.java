aefb5dc6a47f529556ee6bfd0987c106ec7c03b0
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2023-04-13 15:20:10 +0200

    Test

--- td_liste_fifo_lifo/Main.java
+++ td_liste_fifo_lifo/Main.java
@@ -1,25 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        ListeFIFO l = new ListeFIFO();
+        System.out.println("LISTE FIFO");
+        ListeFIFO lFIFO = new ListeFIFO();
         for (int i = 0; i < 5; i++) {
-            l.empiler(new Element(null, i));
+            lFIFO.empiler(new Element(null, i));
         }
-        l.afficherListe();
+        lFIFO.afficherListe();
         for (int i = 0; i < 2; i++) {
-            l.depiler();
+            lFIFO.depiler();
         }
 
-        l.afficherListe();
+        lFIFO.afficherListe();
 
         for (int i = 0; i < 5; i++) {
-            l.empiler(new Element(null, i));
+            lFIFO.empiler(new Element(null, i));
         }
-        l.afficherListe();
+        lFIFO.afficherListe();
 
-        while (!l.estVide()) {
-            l.depiler();
+        while (!lFIFO.estVide()) {
+            lFIFO.depiler();
         }
 
-        l.afficherListe();
+        lFIFO.afficherListe();
+
+        System.out.println("---------------------------------------------");
+        System.out.println("LISTE LIFO");
+        ListeLIFO lLIFO= new ListeLIFO();
+        for (int i = 0; i < 5; i++) {
+            lLIFO.empiler(new Element(null, i));
+        }
+        lLIFO.afficherListe();
+        for (int i = 0; i < 2; i++) {
+            lLIFO.depiler();
+        }
+
+        lLIFO.afficherListe();
+
+        for (int i = 0; i < 5; i++) {
+            lLIFO.empiler(new Element(null, i));
+        }
+        lLIFO.afficherListe();
+
+        while (!lLIFO.estVide()) {
+            lLIFO.depiler();
+        }
+
+        lLIFO.afficherListe();
     }
 }
