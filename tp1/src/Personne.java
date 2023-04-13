6960ad16ba3acea0da8198405eb4dcc54bfb28b2
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2023-04-13 10:57:11 +0200

    Update code

--- tp1/src/Personne.java
+++ tp1/src/Personne.java
@@ -1,2 +1,69 @@
 public class Personne {
+    private String nom;
+    private String prenom;
+    private final String paysNaissance;
+    private final String villeNaissance;
+
+    public Personne(String n, String p, String pn, String vn) {
+        nom = n;
+        prenom = p;
+        paysNaissance = pn;
+        villeNaissance = vn;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPaysNaissance() {
+        return paysNaissance;
+    }
+
+    public String getVilleNaissance() {
+        return villeNaissance;
+    }
+    public void affiche() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return this.getNom()+ ";"+this.getPrenom()+";"+this.getVilleNaissance()+";"+this.getPaysNaissance();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Personne)) {
+            return false;
+        }
+
+        Personne p = (Personne) obj;
+
+        return this.getNom().equals(p.getNom()) && this.getPrenom().equals(p.getPrenom()) && this.getVilleNaissance().equals(p.getVilleNaissance()) && this.getPaysNaissance().equals(p.getPaysNaissance());
+    }
+
+    public static void main(String[] args) {
+        Personne p1 = new Personne("Jean", "Philippe", "Rennes", "France");
+        Personne p2 = new Personne("Toto", "Philippe", "Rennes", "France");
+        Personne p3 = new Personne("Toto", "Philippe", "Rennes", "France");
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.equals(p3));
+        System.out.println(p3.equals(p2));
+
+    }
 }
--- tp1/src/SaisieUtilisateur.java
+++ tp1/src/SaisieUtilisateur.java
@@ -1,2 +1,26 @@
-public class SaisieUtilisateur {
+import java.util.Scanner;
+
+public abstract class SaisieUtilisateur {
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void saisieChaineCaractere() {
+        System.out.println("Saisir une chaine de caractere :");
+        String info = input.next();
+        System.out.println(info);
+    }
+
+    public static void saisieEntiers() {
+        int[] array = new int[2];
+        for(int i = 0; i < array.length; i++) {
+            System.out.println("Saisir un entier :");
+            array[i] = input.nextInt();
+        }
+        System.out.println(array[0]+"*"+array[1]+" = "+(array[0]*array[1]));
+        System.out.println(array[0]+"/"+array[1]+" = "+(float)(array[0]/array[1]));
+    }
+    public static void main(String[] args) {
+        SaisieUtilisateur.saisieEntiers();
+        SaisieUtilisateur.saisieChaineCaractere();
+    }
 }
