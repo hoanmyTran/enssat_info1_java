b3be116ec263fb527640d14e802b8c9a7e694be9
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2023-05-19 18:32:55 +0200

    Update client

--- tp4/Client.java
+++ tp4/Client.java
@@ -7,12 +7,34 @@ public class Client {
     private String rueClient;
     private String villeClient;
 
+    public Client(int numero, String nom, String prenom, int numeroDePorte, String rueClient, String villeClient, int codePostalClient) {
+        this.numero = numero;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numeroDePorte = numeroDePorte;
+        this.rueClient = rueClient;
+        this.villeClient = villeClient;
+        this.codePostalClient = codePostalClient;
+    }
+
     public String getNomClient() {
         return nom;
     }
 
     public String getPrenomClient() {
         return prenom;
+    }
+
+    public String getAdresseClient() {
+        return new StringBuilder()
+                .append(numeroDePorte)
+                .append(" ")
+                .append(rueClient)
+                .append(" ")
+                .append(villeClient)
+                .append(" ")
+                .append(codePostalClient)
+                .toString();
     }
 
     public void setNom(String nom) {
@@ -25,10 +47,19 @@ public class Client {
 
     @Override
     public String toString() {
-        return "";
+        return new StringBuilder()
+                .append(numero)
+                .append(" ")
+                .append(getNomClient())
+                .append(" ")
+                .append(getPrenomClient())
+                .append(" ")
+                .append(getAdresseClient())
+                .toString();
     }
 
     public static void main(String[] args) {
-
+        Client c = new Client(1, "Toto", "Titi", 2, "rue", "Lannion", 22300);
+        System.out.println(c);
     }
 }
