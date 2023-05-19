162aa8084a7400657ac9ca1f429831c747d65190
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2023-05-19 20:15:42 +0200

    Init interface

---
+++ tp4/ParametresCompteEpargne.java
@@ -0,0 +1,5 @@
+public interface ParametresCompteEpargne {
+    public static int PLAFOND = 20000;
+    public static int VERSEMENT_INITIAL = 30;
+    public static int MINIMUM_SUR_LE_COMPTE = 5;
+}
